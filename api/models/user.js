642a8f236248848985ea226ed6c911a02c51c559
victordelajarte <victordelajarte@hotmail.fr>
2019-11-03 16:29:10 +0100

    ajout de propriétés par défaut sur le model de user

--- api/models/user.js
+++ api/models/user.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+const userSchema = mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: { type: String, required: true },
+    nom: { type: String, required: true },
+    prenom: { type: String, required: true },
+    isEmailValidated: { type: Boolean, default: false }
   },
-  password: { type: String, required: true },
-  nom: { type: String, required: true },
-  prenom: { type: String, required: true }
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
