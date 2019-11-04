ea51b782c14d913871c9aabef1aa9486511686cc
victordelajarte <victordelajarte@hotmail.fr>
2019-11-04 01:12:37 +0100

    maj des infos obligatoires et sensibles de user

--- api/models/user.js
+++ api/models/user.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
 
+// Select: false permet de rendre le champ non visible dans les résultats de query par défaut
+// timestamps: true permet d'avoir les timestamps de création et modification enregistrés dans la BDD par défaut
 const userSchema = mongoose.Schema(
   {
-    _id: mongoose.Schema.Types.ObjectId,
+    _id: { type: mongoose.Schema.Types.ObjectId, select: false },
     email: {
       type: String,
       required: true,
       unique: true,
       match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: false },
     nom: { type: String, required: true },
     prenom: { type: String, required: true },
-    isEmailValidated: { type: Boolean, default: false }
+    isEmailValidated: { type: Boolean, default: false, select: false }
   },
   { timestamps: true }
 );
